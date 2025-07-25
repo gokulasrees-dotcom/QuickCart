2712e463088ef21ef68cf4a87896376a81cced55
gokuasrees-dotcom <gokulasree.s@cmr.edu.in>
2025-07-25 09:00:54 +0530

    inngest updated

--- config/inngest.js
+++ config/inngest.js
@@ -7,16 +7,16 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 
 // Inngest function to save user data to DB
 export const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+  { id: 'sync-user-from-clerk' },
+  { event: 'clerk/user.created' },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
     const userData = {
-      _id: id,
-      email: email_addresses?.[0]?.email_address || null,
-      name: `${first_name} ${last_name}`.trim(),
-      imageUrl: image_url,
+      _id:id,
+      email: email_addresses[0].email_address,
+      name: first_name+' '+last_name,
+      imageUrl: image_url
     };
 
     await connectDB();
@@ -26,15 +26,16 @@ export const syncUserCreation = inngest.createFunction(
 
 // Inngest function to update user data in DB
 export const syncUserUpdation = inngest.createFunction(
-  { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  { id: 'update-user-from-clerk' },
+  { event: 'clerk/user.updated' },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
     const userData = {
-      email: email_addresses?.[0]?.email_address || null,
-      name: `${first_name} ${last_name}`.trim(),
-      imageUrl: image_url,
+      _id:id,
+      email: email_addresses[0].email_address,
+      name: first_name+' '+last_name,
+      imageUrl: image_url
     };
 
     await connectDB();
@@ -44,8 +45,8 @@ export const syncUserUpdation = inngest.createFunction(
 
 // Inngest function to delete user from DB
 export const syncUserDeletion = inngest.createFunction(
-  { id: "delete-user-with-clerk" },
-  { event: "clerk/user.deleted" },
+  { id: 'delete-user-with-clerk' },
+  { event: 'clerk/user.deleted' },
   async ({ event }) => {
     const { id } = event.data;
 
